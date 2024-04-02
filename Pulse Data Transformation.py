7089a5159fae0ef372db957b54b198da8ac8e224
Kamalendu Biswas <kamalendu.biswas@databricks.com>
2024-04-02 18:48:46 +0200

    code updated

--- Pulse Data Transformation.py
+++ Pulse Data Transformation.py
@@ -1,19 +1,45 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC ## This notebook is used to transform the pulse data from Repo, copy it to Unity Catalog Volume and Tranform it.
+# MAGIC # This notebook is used to:
+# MAGIC - #### Copy pulse data to Unity Catalog Volume
+# MAGIC - #### Create Bronze layer table
+# MAGIC - #### Transform the pulse data
+# MAGIC - #### Create Silver layer table
 
 # COMMAND ----------
 
-# DBTITLE 1,Download Pulse Data from Git
+# DBTITLE 1,Setup Variables
+# input your repo path
+source_repo_path = "/Workspace/Repos/odl_instructor_1280678@databrickslabs.com/de-training/DE Training Pulse Check (Responses) - Form Responses 1.csv"
+
+# input your catalog name
+my_catalog = "kb_utrecht_training"
+my_volume = "pulse_check"
+
+target_file_path = f"/Volumes/{my_catalog}/bronze/{my_volume}/pulse_data.csv"
+
+# COMMAND ----------
+
+# DBTITLE 1,Create Catalog, Schema and Volume
+catalog_sql = f"CREATE CATALOG IF NOT EXISTS {my_catalog}"
+spark.sql(catalog_sql)
+
+schema_list = ['bronze', 'silver', 'gold']
+for schema in schema_list:
+    schema_sql = f"CREATE SCHEMA IF NOT EXISTS {my_catalog}.{schema}"
+    spark.sql(schema_sql)
+
+volume_sql = f"CREATE VOLUME IF NOT EXISTS {my_catalog}.bronze.{my_volume}"
+spark.sql(volume_sql)
+
+# COMMAND ----------
+
+# DBTITLE 1,Copy raw pulse csv to UC Volume
 import os
 import shutil
 
-source_repo_path = "/Workspace/Repos/odl_instructor_1280678@databrickslabs.com/de-training/DE Training Pulse Check (Responses) - Form Responses 1.csv"
-
-target_volume_path =  "/Volumes/kb_utrecht_training/bronze/pulse_check/pulse_data.csv"
-
 # Move the source file to the target path
-shutil.copy(source_repo_path, target_volume_path)
+shutil.copy(source_repo_path, target_file_path)
 
 # COMMAND ----------
 
@@ -31,10 +57,10 @@ schema = StructType([
 ])
 
 # Read the pulse data csv
-bronze_df = spark.read.options(multiline = True, delimiter=",", header=True).schema(schema).csv(target_volume_path)
+bronze_df = spark.read.options(multiline = True, delimiter=",", header=True).schema(schema).csv(target_file_path)
 
 # Save as bronze table
-bronze_df.write.mode("overwrite").saveAsTable("kb_utrecht_training.bronze.raw_pulse_data")
+bronze_df.write.mode("overwrite").saveAsTable(f"{my_catalog}.bronze.raw_pulse_data")
 
 # COMMAND ----------
 
@@ -55,4 +81,4 @@ silver_df = silver_df.withColumn('rating', split_col.getItem(1))
 transfomed_df = silver_df.withColumn("rating", silver_df["rating"].cast(IntegerType()))
 
 # Save as silver table
-transfomed_df.write.mode("overwrite").saveAsTable("kb_utrecht_training.silver.transformed_pulse_data")
+transfomed_df.write.mode("overwrite").saveAsTable(f"{my_catalog}.silver.transformed_pulse_data")
